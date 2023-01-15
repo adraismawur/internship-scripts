f9dd73f9a06c46eb43e4172931128d5ad4dfa208
Arjan <arjan.draisma@wur.nl>
2023-01-15 22:13:26 +0100

    fix gene family input

--- run_gemcapy.sh
+++ run_gemcapy.sh
@@ -2,5 +2,5 @@
 mkdir $4
 for gbk in $3/* ; do
     file=${gbk##*/}
-    python $1 --macse_path $2 -t $file -i $3/$file -o $4/$file --output_algn $6 $7
+    python $1 --macse_path $2 -t $file -i $3/$file -o $4/$file -g $5/$file/$file\_GeneFamilies.tsv $6 $7
 done
